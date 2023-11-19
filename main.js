24b06b650b277dcadf5d368889d0325ea97723a4
amanyzohair <amanyzohair3@gmail.com>
2023-11-19 22:02:54 +0200

    Changed provider to company

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 function passAttributes() {
   let widget = document.getElementById("estafsarWidget");
-  let provider;
+  let company;
   let channel;
 
   fetch("./assets/data.json")
     .then((response) => response.json())
     .then((data) => {
-      provider = data.provider;
+      company = data.company;
       channel = data.channel;
-      widget.provider = provider;
+
+      widget.company = company;
       widget.channel = channel;
-      console.log(data);
     });
 }
 
